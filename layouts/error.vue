e87cc38bedc9dea2a39a57b34fb1a915f0e76bab
acepnurmansidik <acepnurmansidik@gmail.com>
2022-07-06 06:34:04 +0700

    added Landing page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,36 +1,34 @@
 <template>
-        <div class="container mx-auto h-screen flex justify-center items-center">
-            <div class="w-full lg:w-1/3 px-10 lg:px-0">
-          <div class="flex justify-center items-center mx-auto mt-6 mb-8">
-            <img src="../static/404-illustration.svg" alt="" class="w-full" />
-          </div>
-          <h2 class="font-medium mb-3 text-3xl text-center">
-            Opps! something went wrong.
-          </h2>
-          <p class="text-center font-light" v-if="error.statusCode === 404">
-            The page that you requested doesn't
-            <br />
-            exists that moment
-          </p>
-          <p class="text-center font-light" v-else>
-              An error occured
-          </p>
-          <div class="mb-4 mt-6">
-            <div class="mb-3">
-              <button
-                @click="$router.push({ path: '/' })"
-                class="block w-full bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-4 text-lg rounded-full"
-              >
-                Back to Home
-              </button>
-            </div>
+  <div class="success-page">
+    <div class="container mx-auto h-screen flex justify-center items-center">
+      <div class="w-full lg:w-1/3 px-10 lg:px-0">
+        <div class="flex justify-center items-center mx-auto mt-6 mb-8">
+          <img src="/success-illustration.svg" alt="" class="w-full" />
+        </div>
+        <h2 class="font-medium mb-3 text-3xl text-center">Welcome onboard!</h2>
+        <p class="text-center font-light">
+          Your account just registered
+          <br />
+          into our system
+        </p>
+        <div class="mb-4 mt-6">
+          <div class="mb-3">
+            <button
+              @click="$router.push({ path: '/' })"
+              class="block w-full bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-4 text-lg rounded-full"
+            >
+              Start Explore
+            </button>
           </div>
         </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default{
-    props:["error"],
-    layout:'success'
+export default {
+  props: ['error'],
+  layout: 'success',
 }
 </script>
