7d97aa1f11d69237c55e762cccedee194d3f0ecd
acepnurmansidik <acepnurmansidik@gmail.com>
2022-07-09 02:16:44 +0700

    integrasi konfigurasi autentikasi

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'fe-bwastartup',
+    title: 'BWA Startup',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -49,7 +49,27 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    baseURL: 'http://localhost:8080/api/',
+  },
+
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: {
+            url: '/v1/sessions',
+            method: 'post',
+            propertyName: 'data.token',
+          },
+          loggout: false,
+          user: {
+            url: '/v1/users/fetch',
+            method: 'get',
+            propertyName: 'data',
+          },
+        },
+      },
+    },
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
